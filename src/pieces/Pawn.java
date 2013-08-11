017e3798bc3cf989d28ef49724c6e1f88c441096
happyhj <happyhj@gmail.com>
2013-08-12 01:32:38 +0900

    Pawn 클래스의 getPossibleMoves() 메소드를 구현, 시작위치고려

--- src/pieces/Pawn.java
+++ src/pieces/Pawn.java
@@ -15,10 +15,18 @@ public class Pawn extends Piece {
 		// 흰색일 경우 북쪽으로 한칸 이동가능
 		if((super.color==Color.WHITE)&&(super.position.move(Direction.NORTH).isValid())) {
 			positions.add(super.position.move(Direction.NORTH));
+			// 시작위치일 경우 두칸이동가능
+			if(super.position.getY()==1){
+				positions.add(super.position.move(Direction.NORTH).move(Direction.NORTH));	
+			}
 		}
 		// 검색일 경우 남쪽으로 한칸 이동가능
 		else if((super.color==Color.BLACK)&&(super.position.move(Direction.SOUTH).isValid())) {
-			positions.add(super.position.move(Direction.SOUTH));			
+			positions.add(super.position.move(Direction.SOUTH));
+			// 시작위치일 경우 두칸이동가능
+			if(super.position.getY()==6){
+				positions.add(super.position.move(Direction.SOUTH).move(Direction.SOUTH));	
+			}
 		}
 		return positions;
 	}
--- test/pieces/PawnTest.java
+++ test/pieces/PawnTest.java
@@ -23,4 +23,35 @@ public class PawnTest extends TestCase {
 		assertEquals(1, bPawnPossibleMoves.size());	
 		assertEquals(position.move(Direction.SOUTH), bPawnPossibleMoves.get(0));	
 	}
+	
+	public void testGetPossibleMovesOfPawnAtStartingPoint() throws Exception {
+		int startX = 4;
+		int startY1 = 6;
+		int startY2 = 1;
+
+		Position position1 = new Position(startX, startY1);		
+		Position position2 = new Position(startX, startY2);		
+
+		Pawn wPawn1 = new Pawn(Color.WHITE, position2);		
+		Pawn wPawn2 = new Pawn(Color.WHITE, position1);		
+
+		Pawn bPawn1 = new Pawn(Color.BLACK, position1);		
+		Pawn bPawn2 = new Pawn(Color.BLACK, position2);		
+
+		List<Position> wPawnPossibleMoves1 = wPawn1.getPossibleMoves();		
+		List<Position> wPawnPossibleMoves2 = wPawn2.getPossibleMoves();		
+		
+		assertEquals(2, wPawnPossibleMoves1.size());	
+		assertEquals(position2.move(Direction.NORTH).move(Direction.NORTH), wPawnPossibleMoves1.get(1));	
+		assertEquals(1, wPawnPossibleMoves2.size());	
+		assertEquals(position1.move(Direction.NORTH), wPawnPossibleMoves2.get(0));	
+
+		List<Position> bPawnPossibleMoves1 = bPawn1.getPossibleMoves();		
+		List<Position> bPawnPossibleMoves2 = bPawn2.getPossibleMoves();		
+		
+		assertEquals(2, bPawnPossibleMoves1.size());	
+		assertEquals(position1.move(Direction.SOUTH).move(Direction.SOUTH), bPawnPossibleMoves1.get(1));	
+		assertEquals(1, bPawnPossibleMoves2.size());	
+		assertEquals(position2.move(Direction.SOUTH), bPawnPossibleMoves2.get(0));			
+	}
 }
